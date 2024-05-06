6dec3bf37835733c0b273aa8d1e75375871bc139
drewjenks01 <awj@mit.edu>
2024-05-06 00:08:54 -0400

    testing

--- database/db.py
+++ database/db.py
@@ -10,43 +10,46 @@ class Database:
         self.input_dim = 512
 
         if self.search_method == "lsh": #initialize lsh
-            self.lsh = lshash.LSHash(32, self.input_dim)
+            self.lsh = lshash.LSHash(6, self.input_dim, 5)
         elif self.search_method == "hnsw": #initialize hnsw
+            self.embedding_list = []
             if self.dist_func == "l2_squared":
                 self.hnsw = hnswlib.Index(space='l2', dim=self.input_dim)
             elif self.dist_func == "cosine":
                 self.hnsw = hnswlib.Index(space='cosine', dim=self.input_dim)
-            self.hnsw.init_index(max_elements=5, ef_construction=3, M=50) 
+            self.hnsw.init_index(max_elements=200, ef_construction=3, M=50) 
             self.hnsw.set_ef(3)
         elif self.search_method == "vector_compression": #initialize vector compression dependencies
             self.projection_mat = np.random.randn(32, self.input_dim)
             self.compressed_mat = None
 
     def add_entry(self, embedding, info):
-        self.table[embedding] = info
+        self.table[tuple(embedding.tolist())] = info
         if self.search_method == "lsh":
             self.lsh.index(embedding)
         elif self.search_method == "hnsw":
             self.hnsw.add_items(embedding)
+            self.embedding_list.append(tuple(embedding.tolist()))
         elif self.search_method == "vector_compression":
-            compressed_vec = np.dot(self.projection_matrix, embedding.T).T
+            compressed_vec = np.dot(self.projection_mat, embedding.T).T
             if self.compressed_mat is None:
-                self.compressed_mat = compressed_vec
+                self.compressed_mat = compressed_vec.reshape(1,-1)
             else:
-                self.compressed_mat = np.concatenate(self.compressed_mat, compressed_vec, axis=0)
+                self.compressed_mat = np.concatenate((self.compressed_mat, compressed_vec.reshape(1,-1)), axis=0)
 
     def query_entry(self, query_vector) -> list:
         if self.search_method == "lsh":
             if self.dist_func == "l2_squared":
-                vec = lshash.query(query_vector, num_results=1, distance_func="euclidean_dist_square")[0][0]
+                vec = self.lsh.query(query_vector, num_results=1, distance_func="euclidean")[0][0][0]
             elif self.dist_func == "cosine":
-                vec = lshash.query(query_vector, num_results=1, distance_func="cosine")[0][0]
+                vec = self.lsh.query(query_vector, num_results=1, distance_func="cosine")[0][0][0]
         elif self.search_method == "hnsw":
-            vec = self.hnsw.knn_query(query_vector, k=1)[0][0]
+            labels, distances = self.hnsw.knn_query(query_vector, k=1)
+            vec = self.embedding_list[labels[0][0]]
 
         elif self.search_method == "vector_compression":
             # projected_query = np.dot(self.projection_matrix, query_vector)
-            projected_query = np.dot(self.projection_matrix, query_vector.T).T
+            projected_query = np.dot(self.projection_mat, query_vector.T).T
             if self.dist_func == "l2_squared":
                 similarities = np.sum((self.compressed_mat - projected_query) ** 2, axis=1)
             elif self.dist_func == "cosine":
@@ -54,11 +57,14 @@ class Database:
                     np.linalg.norm(self.compressed_mat, axis=1) * np.linalg.norm(projected_query)
                 )
             vec_index = np.argmax(similarities)
-            vec = np.dot(self.compressed_mat[vec_index], np.linalg.pinv(self.projection_matrix))
+            vec = self.compressed_mat[vec_index].reshape(1,-1)/np.linalg.norm(self.projection_mat)
+            vec = np.linalg.lstsq(self.projection_mat, self.compressed_mat[vec_index].T)[0].T
+            vec = tuple(vec.tolist())
+            # vec = tuple(np.dot(self.compressed_mat[vec_index].reshape(1,-1), np.linalg.pinv(self.projection_mat).T)[0].tolist())
 
         elif self.search_method== "linear":
-            vec, best_distance = None, "inf"
-            if self.dist_func == "euclidean":     
+            vec, best_distance = None, np.inf
+            if self.dist_func == "l2_squared":     
                 for vector in self.table:
                     squared_difference = np.sum((np.array(query_vector) - np.array(vector)) ** 2)
                     if squared_difference < best_distance:
